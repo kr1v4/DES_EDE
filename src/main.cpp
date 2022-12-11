775e8027f9e74add0d22c3e36396de59d20687ff
kr1v4 <kr1va.is.watching.you@gmail.com>
2022-12-11 18:10:42 +0300

    created utils class and implemented converting methods, created des class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
-#include "keygen/keygen.hpp"
+#include "des/des.hpp"
 
 int main()
 {
-
-	bitarray input_key("0001001100110100010101110111100110011011101111001101111111110001");
-
-	std::vector<bitarray> temp = keygen::generate_round_keys(input_key);
-
-	for (auto& key : temp)
-	{
-		std::cout << key << "\n";
-	}
+	des::process("hello", "hello");
 }
